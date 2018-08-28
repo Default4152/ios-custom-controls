cb97af876f153a3bbfc9d0a81e8f8a26bb51132b
Conner Alegre <cga0@pm.me>
2018-08-28 14:19:50 -0600

    add control view logic

--- ios-custom-controls/CustomControl.swift
+++ ios-custom-controls/CustomControl.swift
@@ -9,6 +9,60 @@
 import UIKit
 
 class CustomControl: UIControl {
+  required init?(coder aCoder: NSCoder) {
+    super.init(coder: aCoder)
+    setup()
+  }
+  
   var value: Int = 1
+  
   private let componentDimension: CGFloat = 40.0
+  private let componentCount = 5
+  private let componentActiveColor = UIColor.black
+  private let componentInactiveColor = UIColor.gray
+  
+  func setup() {
+    let labels = [UILabel(frame: CGRect(x: 0,
+                                        y: 0,
+                                        width: componentDimension,
+                                        height: componentDimension)),
+                  UILabel(frame: CGRect(x: 27.0,
+                                        y: 0,
+                                        width: componentDimension,
+                                        height: componentDimension)),
+                  UILabel(frame: CGRect(x: 55.0,
+                                        y: 0,
+                                        width: componentDimension,
+                                        height: componentDimension)),
+                  UILabel(frame: CGRect(x: 83.0,
+                                        y: 0,
+                                        width: componentDimension,
+                                        height: componentDimension)),
+                  UILabel(frame: CGRect(x: 111.0,
+                                        y: 0,
+                                        width: componentDimension,
+                                        height: componentDimension))]
+    
+    for (i, label) in labels.enumerated() {
+      label.text = "★"
+      label.tag = i
+      label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+      
+      if (i == 0) {
+        label.textColor = componentActiveColor
+      } else {
+        label.textColor = componentInactiveColor
+      }
+      
+      self.addSubview(label)
+    }
+  }
+  
+  override var intrinsicContentSize: CGSize {
+    let componentsWidth = CGFloat(componentCount) * componentDimension
+    let componentsSpacing = CGFloat(componentCount + 1) * 8.0
+    let width = componentsWidth + componentsSpacing
+    return CGSize(width: width, height: componentDimension)
+  }
+  
 }
