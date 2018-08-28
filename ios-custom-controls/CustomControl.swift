e17577f6b1222f0cdaaedd3d9c009541b7782727
Conner Alegre <cga0@pm.me>
2018-08-28 14:57:17 -0600

    add in touch event tracking

--- ios-custom-controls/CustomControl.swift
+++ ios-custom-controls/CustomControl.swift
@@ -67,4 +67,46 @@ class CustomControl: UIControl {
     return CGSize(width: width, height: componentDimension)
   }
   
+  func updateValue(at touch: UITouch) {
+    let labels = self.subviews.compactMap { $0 as? UILabel }
+    
+    for label in labels {
+      print(label)
+      print(touch)
+    }
+  }
+  
+  override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+    let touchPoint = touch.location(in: self)
+    updateValue(at: touch)
+    return true
+  }
+  
+  override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+    let touchPoint = touch.location(in: self)
+    if bounds.contains(touchPoint) {
+      updateValue(at: touch)
+      sendActions(for: .touchDragInside)
+    } else {
+      sendActions(for: .touchDragOutside)
+    }
+    
+    return true
+  }
+  
+  override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+    guard let touch = touch else { return }
+    let touchPoint = touch.location(in: self)
+    if bounds.contains(touchPoint) {
+      updateValue(at: touch)
+      sendActions(for: .touchUpInside)
+    } else {
+      sendActions(for: .touchUpOutside)
+    }
+  }
+  
+  override func cancelTracking(with event: UIEvent?) {
+    sendActions(for: .touchCancel)
+  }
+  
 }
